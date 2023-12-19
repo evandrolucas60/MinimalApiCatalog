7ef28dd8daa1c1b98971337124d0938cb215e0e4
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-19 15:15:34 -0300

    Creating the Context file - AppDbContext and defining the connection string

---
+++ MinimalApiCatalog/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiCatalog.Models;
+
+namespace MinimalApiCatalog.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
