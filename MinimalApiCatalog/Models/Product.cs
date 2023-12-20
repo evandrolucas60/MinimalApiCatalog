ec10402db967d997a3ec675be5aff083c8053b2f
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-20 10:47:46 -0300

    Creating endpoints for Product

--- MinimalApiCatalog/Models/Product.cs
+++ MinimalApiCatalog/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace MinimalApiCatalog.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MinimalApiCatalog.Models
 {
     public class Product
     {
@@ -11,6 +13,8 @@
         public int Stock { get; set; }
 
         public int CategoryId { get; set; }
+
+        [JsonIgnore]
         public Category? Category { get; set; }
     }
 }
--- MinimalApiCatalog/Program.cs
+++ MinimalApiCatalog/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalApiCatalog.Context;
 using MinimalApiCatalog.Models;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-//definir os endpoints
+// ------------------------endpoints Category ---------------------------------
 app.MapPost("/categorias", async(Category category, ApplicationDbContext db) =>
 {
     db.Categories.Add(category);
@@ -60,6 +61,66 @@ app.MapDelete("/categoria/{id:int}", async (int id, ApplicationDbContext db) =>
     }
 
     db.Categories.Remove(category);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+
+// ------------------------endpoints Product ---------------------------------
+app.MapPost("/produtos", async (Product product, ApplicationDbContext db)
+ => {
+     db.Products.Add(product);
+await db.SaveChangesAsync();
+
+return Results.Created($"/produtos/{product.ProductId}", product);
+ });
+
+app.MapGet("/produtos", async (ApplicationDbContext db) => await db.Products.ToListAsync());
+
+app.MapGet("/produtos/{id:int}", async (int id, ApplicationDbContext db)
+    => {
+        return await db.Products.FindAsync(id)
+                     is Product product
+                     ? Results.Ok(product)
+                     : Results.NotFound();
+    });
+
+app.MapPut("/produtos/{id:int}", async (int id, Product product, ApplicationDbContext db) =>
+{
+
+    if (product.ProductId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var productDB = await db.Products.FindAsync(id);
+
+    if (productDB is null) return Results.NotFound();
+
+    productDB.Name = product.Name;
+    productDB.Description = product.Description;
+    productDB.Price = product.Price;
+    productDB.Image = product.Image;
+    productDB.PurchaseDate = product.PurchaseDate;
+    productDB.Stock = product.Stock;
+    productDB.CategoryId = product.CategoryId;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(productDB);
+});
+
+app.MapDelete("/produtos/{id:int}", async (int id, ApplicationDbContext db) =>
+{
+    var product = await db.Products.FindAsync(id);
+
+    if (product is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Products.Remove(product);
     await db.SaveChangesAsync();
 
     return Results.NoContent();
